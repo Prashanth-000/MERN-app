d27f1429fa7e2fd7df412251593d521ba73e69db
prashanth <151809864+Prashanth-000@users.noreply.github.com.>
2025-10-26 02:17:34 +0530

    modified env

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
 // Use environment variables for the API URL
-const BASE_URL = process.env.VITE_API_URL;
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 
 const api = axios.create({
   baseURL: BASE_URL,
